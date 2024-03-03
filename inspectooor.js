514005060257799495f384b03452da2f4f4743ff
keosariel <kennethgabriel78@gmail.com>
2024-03-03 14:28:48 +0100

    fix: toolbar

--- inspectooor.js
+++ inspectooor.js
@@ -4,13 +4,25 @@
  * Kenneth Gabriel <gabriel@ally.wtf>
  */
 
-function baseStyles() {
-  let style = document.createElement("style");
-  style.innerHTML = `
-    body * { cursor: pointer !important; }
-    selector > div { transition: all 300ms ease; }
-  `;
-  return style;
+function generateToolbarHTML() {
+  let toolbar = document.createElement("div");
+  toolbar.id = "toolbar";
+  toolbar.innerHTML = `
+    <button id="inspect">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="#fff"
+            viewBox="0 0 16 16"
+        >
+            <path
+                d="M8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v5.34l-1.2.24a1.5 1.5 0 0 0-1.196 1.636l.345 3.106a2.5 2.5 0 0 0 .405 1.11l1.433 2.15A1.5 1.5 0 0 0 6.035 16h6.385a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5 5 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.6 2.6 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046z"
+            />
+        </svg>
+    </button>
+    `;
+  return toolbar;
 }
 
 function generateSelectorHTML() {
@@ -292,9 +304,9 @@ function main() {
   const body = $("body");
 
   // Add the mainmenu and selector to the body
-  html.append(baseStyles());
   body.prepend(generateMainMenuHTML());
   body.prepend(generateSelectorHTML());
+  body.prepend(generateToolbarHTML());
 
   const mainmenu = $("#mainmenu");
   const htmlProps = $("#htmlProps");
@@ -310,11 +322,21 @@ function main() {
 
   html.addClass("relative");
   html.on("click", function (e) {
+    let shouldInspect = body.attr("data-inspect") === "true";
+
+    if (!shouldInspect) {
+      return;
+    }
+
     if (
       !mainmenu.contains(e.target) &&
       e.target.tagName !== "HTML" &&
       mainmenu.css("display") === "none"
     ) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+
       let position = generateMenuPosition(e, mainmenu);
       let generatedId = generateId();
       let oldGeneratedId = e.target.getAttribute("data-cid");
